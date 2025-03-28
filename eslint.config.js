d12c99e7aebb2c113fc113cf2fe57a9a8e1cffb3
Bakhtiar Ismail <bakhtiar.khiderismail@edu.turkuamk.fi>
2025-03-28 16:56:46 +0200

    Updating with missing some code in files

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,17 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
-import js from "@eslint/js";
 
-export default defineConfig({
-  overrides: [
-    {
-      files: ["**/*.{js,mjs,cjs}"],
-      languageOptions: {
-        globals: {
-          ...globals.browser, // Use the browser globals
-        },
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Use the browser globals
       },
     },
-    {
-      files: ["**/*.{js,mjs,cjs}"],
-      plugins: {
-        js, // ESLint plugin for JavaScript (recommended rules)
-      },
-      extends: ["plugin:js/recommended"], // Use recommended rules for JavaScript
-    },
-  ],
-});
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    extends: ["eslint:recommended"], // Use ESLint's built-in recommended configuration
+  },
+]);
